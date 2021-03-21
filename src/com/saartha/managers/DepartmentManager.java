ba13e1b088f0134f5f1762655f4afd3e8ae82b30
Sunidhi-21 <sunidhi.n.xworkz@gmail.com>
2021-03-21 10:08:12 +0530

    ver-2 dept table

--- src/com/saartha/managers/DepartmentManager.java
+++ src/com/saartha/managers/DepartmentManager.java
@@ -4,21 +4,24 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 import com.saartha.beans.DepartmentBean;
 import com.saartha.interfaces.Department;
 
 public class DepartmentManager implements Department {
-
+	
+	private static Scanner sc=new Scanner(System.in);
 	private PreparedStatement pstmt;
-
+	private PreparedStatement pstmt0;
 	@Override
 	public void saveDept(int deptId, String deptName, int college, int deptHodId, String hodName, String pwd) {
 
 		DepartmentBean deptBean = new DepartmentBean(deptId, deptName, college, deptHodId, hodName, pwd);
 		String s = "insert into dept values(?,?,?,?,?,?)";
+		String s1= "select clgId from college where clgName='sce'";
 		try {
-
+			
 			int dId = deptBean.getDeptId();
 			String dName = deptBean.getDeptName();
 			int clg = deptBean.getCollege();
@@ -26,12 +29,22 @@ public class DepartmentManager implements Department {
 			String hoDName = deptBean.getHodName();
 			String pass = deptBean.getPwd();
 
+			
 			Connection connectionProvider = ConnectionProvider.getConnectionProvider();
 			System.out.println(connectionProvider);
+			//System.out.println("enter the college name");
+			//String cName=sc.next();
+			pstmt0 = connectionProvider.prepareStatement(s1);
+			ResultSet rs = pstmt0.executeQuery();
+			int clg1=0;
+			if (rs.next()) {
+				clg1=rs.getInt(1);
+				System.out.println(clg1);
+				}
 			pstmt = connectionProvider.prepareStatement(s);
 			pstmt.setInt(1, dId);
 			pstmt.setString(2, dName);
-			pstmt.setInt(3, clg);
+			pstmt.setInt(3, clg1);
 			pstmt.setInt(4, deptHoDId);
 			pstmt.setString(5, hoDName);
 			pstmt.setString(6, pass);
@@ -49,7 +62,7 @@ public class DepartmentManager implements Department {
 	@Override
 	public void fetchDept() {
 
-		String s = "select * from dept";
+		String s = "select * from dept ";
 		Connection connectionProvider = ConnectionProvider.getConnectionProvider();
 		System.out.println(connectionProvider);
 		try {
